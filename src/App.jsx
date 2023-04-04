6052351d09763afdd31185e51270a56433bcd41e
qnhn2 <nhatquan234@gmail.com>
2023-04-04 01:59:04 -0400

    fetch data from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import axios from 'axios'
+import CoinInfo from './components/CoinInfo'
 
 function App() {
   const [list, setList] = useState(null)
+  const [filteredResult, setFilteredResult] = useState([])
+  const [searchInput, setSearchInput] = useState('')
 
   const URL = "https://api.coingecko.com/api/v3/coins/list"
 
@@ -15,12 +18,13 @@ function App() {
     try {
       const coinList = []
       const res = await axios.get(URL)
-      console.log(res.data)
-      for (let i = 0; i < 20; i++) {
+
+      for (let i = 0; i < 50; i++) {
         if (!coinList.includes(res.data[randNum(res.data.length)])) {
           coinList.push(res.data[randNum(res.data.length)])
         }
       }
+
       console.log(coinList)
       setList(coinList)
     } catch (err) {
@@ -32,12 +36,48 @@ function App() {
     return Math.floor(Math.random() * length);
   }
 
+  const searchItems = (searchValue) => {
+    setSearchInput(searchValue)
+    if (searchValue !== '') {
+      const filteredData = list.filter((coin) => {
+        console.log(coin.name)
+        return coin.name.toLowerCase()
+          .includes(searchValue.toLowerCase())
+      })
+      setFilteredResult(filteredData)
+    }
+  }
+
   return (
     <div className="App">
-      <h1>Discover Crypto</h1>
-      {list && list.map((coin) => (
-        <div>{coin.item.name}</div>
-      ))}
+      <h1>Discover Random Cryptocurrencies</h1>
+      <input
+        type='text'
+        placeholder='Search your coin ...'
+        onChange={(e) => searchItems(e.target.value)}
+        value={searchInput}
+      />
+      {searchInput.length > 0 ? (
+        <ul>
+          {filteredResult && filteredResult.map((coin) => (
+            <CoinInfo
+              name={coin.name}
+              id={coin.id}
+              symbol={coin.symbol}
+            />
+          ))}
+        </ul>
+      ) : (
+        <ul>
+          {list && list.map((coin) => (
+            <CoinInfo
+              name={coin.name}
+              id={coin.id}
+              symbol={coin.symbol}
+            />
+          ))}
+        </ul>
+      )}
     </div>
   )
 }
--- src/components/coinInfo.jsx
+++ src/components/coinInfo.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
-function coinInfo() {
+function CoinInfo({ name, id, symbol }) {
+    const [coin, setCoin] = useState(null)
+
+    const URL = `https://api.coingecko.com/api/v3/coins/${id}`
+
+    useEffect(() => {
+        getCoin().catch(console.error)
+    }, [id])
+
+    const getCoin = async () => {
+        const res = await axios.get(URL)
+        console.log(res)
+        setCoin(res.data)
+    }
+
     return (
-        <div>coinInfo</div>
+        <div className='CoinInfo'>
+            {coin && (
+                <li className='main-list' key={id}>
+                    <img
+                        className='icon'
+                        src={coin.image.small}
+                        alt={`icon for ${name}`}
+                    />
+                    {name}({symbol.toUpperCase()})<span className="tab"></span> {coin.market_data.current_price.usd ? coin.market_data.current_price.usd : 'no_value'} USD
+                </li>
+            )
+            }
+        </div>
     )
 }
 
-export default coinInfo
+export default CoinInfo
